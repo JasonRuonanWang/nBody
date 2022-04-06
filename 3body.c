1e35d56e817f902a15482cf6c98bd112ccdc1895
Jason Wang <w4g@ornl.gov>
2022-04-06 03:28:18 -0400

    distance bound

--- 3body.c
+++ 3body.c
@@ -8,12 +8,12 @@ char mode='x';
 char figure='r';
 
 double BOUNDARY = 100.0;
-double BOUNDARY2v1 = 10000;
+double BOUNDARY_C = 100;
 double RND_P = 10.0;
 double RND_V = 5.0;
 
-double c=2;
-double cv=2.2;
+double c=10;
+double cv=2.4;
 double cm=1e12;
 
 double random_double(double max){
@@ -114,37 +114,17 @@ void Judge(){
 
 //    printf("s01=%f, s02=%f, s12=%f\n", s01, s02, s12);
 
-    double s0102 = fabs(s01 - s02);
-    double s0112 = fabs(s01 - s12);
-    double s0212 = fabs(s02 - s12);
+    double s0102 = s01/s02;
+    double s0112 = s01/s12;
+    double s0212 = s02/s12;
 
-    if(s0102 < 0.001) s0102 = 0.001;
-    if(s0112 < 0.001) s0112 = 0.001;
-    if(s0212 < 0.001) s0212 = 0.001;
+    if(s0102<1) s0102=1.0/s0102;
+    if(s0112<1) s0112=1.0/s0112;
+    if(s0212<1) s0212=1.0/s0212;
 
 //    printf("s0102=%f, s0112=%f, s0212=%f\n", s0102, s0112, s0212);
 
-    double ss0 = s0102 / s0112;
-    double ss1 = s0102 / s0212;
-    double ss2 = s0112 / s0212;
-
-//    printf("ss0=%f, ss1=%f, ss2=%f\n", ss0, ss1, ss2);
-
-    if(ss0<1) ss0 = 1 / ss0;
-    if(ss1<1) ss1 = 1 / ss1;
-    if(ss2<1) ss2 = 1 / ss2;
-
-//    printf("ss0=%f, ss1=%f, ss2=%f\n", ss0, ss1, ss2);
-
-    if(ss0 > BOUNDARY2v1 && ss1 > BOUNDARY2v1){
-        dump();
-        reset();
-    }
-    else if(ss0 > BOUNDARY2v1 && ss2 > BOUNDARY2v1){
-        dump();
-        reset();
-    }
-    else if(ss1 > BOUNDARY2v1 && ss2 > BOUNDARY2v1){
+    if(s0102 > BOUNDARY_C || s0112 > BOUNDARY_C || s0212 > BOUNDARY_C ){
         dump();
         reset();
     }
