9956f5b5c0ac2166f84f8e699a065c03f916bf8c
Jason Wang <w4g@ornl.gov>
2022-04-05 20:27:22 -0400

    added fileloader.c

---
+++ fileloader.c
@@ -0,0 +1,14 @@
+#include "nbody.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void Judge(){}
+void reset(){}
+
+int main(int argc, char**argv){
+    return nbody_main(argc,argv);
+}
+
+
